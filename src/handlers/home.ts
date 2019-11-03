bd29d295f95e7d7a55f3c694c380e906bad6143f
sota1235 <sota1235@gmail.com>
2019-11-03 14:07:02 +0900

    moidfy: remake to use webpack

--- src/handlers/home.ts
+++
@@ -1,5 +0,0 @@
-import { Handler } from 'express';
-
-const home: Handler = (_, res) => res.send('hello world');
-
-export { home };
--- src/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Express } from 'express';
-import { home } from './handlers/home';
-
-const setRoutes = (app: Express) => {
-  app.get('/', home);
-};
-
-export { setRoutes };
